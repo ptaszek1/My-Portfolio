5578bd3ad862291a59eae3470ad9664fc48ccc07
Mariusz Ptaszek <mariusz.ptaszek2@gmail.com>
2018-05-13 00:43:57 +0200

    Finished portfolio , added contact form

---
+++ formularz/scripts.js
@@ -0,0 +1,128 @@
+{
+    const form = document.querySelector('#contactForm');
+    const inputs = form.querySelectorAll('input[required], textarea[required], select[required]');
+
+    //wyłączamy domyślną walidację
+    form.setAttribute('novalidate', true);
+
+    const displayFieldError = function(elem) {
+        const fieldRow = elem.closest('.form-row');
+        const fieldError = fieldRow.querySelector('.field-error');
+        if (fieldError === null) {
+            const errorText = elem.dataset.error;
+            const divError = document.createElement('div');
+            divError.classList.add('field-error');
+            divError.innerText = errorText;
+            fieldRow.appendChild(divError);
+        }
+    };
+
+    const hideFieldError = function(elem) {
+        const fieldRow = elem.closest('.form-row');
+        const fieldError = fieldRow.querySelector('.field-error');
+        if (fieldError !== null) {
+            fieldError.remove();
+        }
+    };
+
+    [...inputs].forEach(elem => {
+        elem.addEventListener('input', function() {
+            if (!this.checkValidity()) {
+                this.classList.add('error');
+            } else {
+                this.classList.remove('error');
+                hideFieldError(this);
+            }
+        });
+
+        if (elem.type === "checkbox") {
+            elem.addEventListener('click', function() {
+                const formRow = this.closest('.form-row');
+                if (this.checked) {
+                    this.classList.remove('error');
+                    hideFieldError(this);
+                } else {
+                    this.classList.add('error');
+                }
+            });
+        }
+    });
+
+    const checkFieldsErrors = function(elements) {
+        //ustawiamy zmienną na true. Następnie robimy pętlę po wszystkich polach
+        //jeżeli któreś z pól jest błędne, przełączamy zmienną na false.
+        let fieldsAreValid = true;
+
+        [...elements].forEach(elem => {
+            if (elem.checkValidity()) {
+                hideFieldError(elem);
+                elem.classList.remove('error');
+            } else {
+                displayFieldError(elem);
+                elem.classList.add('error');
+                fieldsAreValid = false;
+            }
+        });
+
+        return fieldsAreValid;
+    };
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        //jeżeli wszystkie pola są poprawne...
+        if (checkFieldsErrors(inputs)) {
+            const elements = form.querySelectorAll('input:not(:disabled), textarea:not(:disabled), select:not(:disabled)');
+
+            const dataToSend = new FormData();
+            [...elements].forEach(el => dataToSend.append(el.name, el.value));
+
+            const url = form.getAttribute('action');
+            const method = form.getAttribute('method');
+
+            const submit = form.querySelector('[type="submit"]');
+            submit.disabled = true;
+            submit.classList.add('element-is-busy');
+
+            fetch(url, {
+                method: method.toUpperCase(),
+                body: dataToSend
+            })
+            .then(ret => ret.json())
+            .then(ret => {
+                submit.disabled = false;
+                submit.classList.remove('element-is-busy');
+
+                if (ret.errors) {
+                    ret.errors.map(function(el) {
+                        return '[name="'+el+'"]'
+                    });
+                    const selector = ret.errors.join(',');
+                    checkFieldsErrors(form.querySelectorAll(sekector));
+
+                } else {
+                    if (ret.status === 'ok') {
+                        //wyświetlamy komunikat powodzenia, cieszymy sie
+                        const div = document.createElement('div');
+                        div.classList.add('form-send-success');
+                        div.innerText = 'Wysłanie wiadomości się nie powiodło';
+
+                        form.parentElement.insertBefore(div, form);
+                        div.innerHTML = '<strong>Wiadomość została wysłana</strong><span>Dziękujemy za kontakt. Postaramy się odpowiedzieć jak najszybciej</span>';
+                        form.remove();
+                    }
+
+                    if (ret.status === 'error') {
+                        //komunikat błędu, niepowodzenia
+                        const div = document.createElement('div');
+                        div.classList.add('send-error');
+                        div.innerText = 'Wysłanie wiadomości się nie powiodło';
+                    }
+                }
+            }).catch(_ => {
+                submit.disabled = false;
+                submit.classList.remove('element-is-busy');
+            });
+        }
+    });
+}
---
+++ send-script.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(0);
+
+$mailToSend = 'contact@mariuszptaszek.pl';
+
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+	$name       = $_POST['name'];
+	$email      = $_POST['email'];
+	$message    = $_POST['message'];
+	$errors     = Array();
+	$return     = Array();
+
+	if ( empty( $name ) ) {
+		array_push( $errors, 'name' );
+	}
+	if ( ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
+		array_push( $errors, 'email' );
+	}
+	if ( empty( $message ) ) {
+		array_push( $errors, 'message' );
+	}
+
+	if ( count( $errors ) > 0 ) {
+		$return['errors'] = $errors;
+	} else {
+		$headers = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+		$headers .= 'From: contact@mariuszptaszek.pl'. "\r\n";
+		$headers .= 'Reply-to: ' . $email;
+		$message = "
+			<html>
+			<head>
+			<meta charset=\"utf-8\">
+			</head>
+			<style type='text/css'>
+				body {font-family:sans-serif; color:#222; padding:20px;}
+				div {margin-bottom:10px;}
+				.msg-title {margin-top:30px;}
+			</style>
+			<body>
+			<div>Imię: <strong>$name</strong></div>
+			<div>Email: <a href=\"mailto:$email\">$email</a></div>
+			<div class=\"msg-title\"> <strong>Wiadomość:</strong></div>
+			<div>$message</div>
+			</body>
+			</html>";
+
+		if ( mail( $mailToSend, 'Wiadomość ze strony - Portfolio' . date( "d-m-Y" ), $message, $headers ) ) {
+			$return['status'] = 'ok';
+		} else {
+			$return['status'] = 'error';
+		}
+	}
+
+	header( 'Content-Type: application/json' );
+	echo json_encode( $return );
+}
